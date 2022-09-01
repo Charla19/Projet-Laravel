e6a3e5ccc4a73417cc3d2e1707d837e569975251
Charla19 <charlap20.aps2a@gmail.com>
2022-09-01 05:01:40 +0300

    front maj

--- dashboard/resources/views/dashboard.blade.php
+++ dashboard/resources/views/dashboard.blade.php
@@ -57,6 +57,42 @@
           <div id="ch4" class="ht-50 tr-y-1"></div>
         </div>
       </div><!-- col-3 -->
-    </div>row
+    </div>
+
+    <br> <br>
+
+    <div>
+    <h1>Liste des Personnels</h1> <br>
+    <table class="table">  
+        <thead class="thead-dark">  
+            <tr>  
+                <th scope="col">#</th>  
+                <th scope="col">Nom/Prénom</th>  
+                <th scope="col">Poste</th>  
+                <th scope="col">Contact</th>  
+            </tr>  
+        </thead>  
+        <tbody>  
+            <tr>  
+                <th scope="row">1</th>  
+                <td>Mark</td>  
+                <td>Otto</td>  
+                <td>@mdo</td>  
+            </tr>  
+            <tr>  
+                <th scope="row">2</th>  
+                <td>Jacob</td>  
+                <td>Thornton</td>  
+                <td>@fat</td>  
+            </tr>  
+            <tr>  
+                <th scope="row">3</th>  
+                <td>Larry</td>  
+                <td>the Bird</td>  
+                <td>@twitter</td>  
+            </tr>  
+        </tbody> 
+    </table>  
+</div>
 
 @stop
